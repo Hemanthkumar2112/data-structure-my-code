1b43b8e51c6a0eb712dbbbd517377db182e29fe8
Hemanthkumar2112 <hemanthmurugan21@gmail.com>
2022-08-14 20:47:54 +0530

    linked list

--- data structure my code/linked_list.py
+++ data structure my code/linked_list.py
@@ -1,34 +1,49 @@
-from random import randrange
-from xml.dom.minidom import Element
-
-
-class linked_list:
-    def __init__(self ,element) -> None:
-        self.element = element 
+class Node:
+    def __init__(self , val):
+        self.val = val
         self.next = None
-
-
-def insert(root,item):
-    temp = linked_list(item)
-    if root == None:
-        root = temp
-    else:
-        temp2 = root
-        while temp2.next !=None:
-            temp2 = temp2.next
-        temp2.next = temp
-    return root
-
-def display(root):
-    while root.next !=None:
-        print(root.element , root.next)
-        root = root.next
-
-###test
-root = None
-for i in range(10):
-    root = insert(root,i)
-
-display(root=root)
-
+    def getItem(self):
+        return self.val
+    def getNext(self):
+        return self.next
+    
+    
+class LinkedList:
+    def __init__(self ,head = None):
+        self.head = None
+        self.count = 0
         
+    def lenght(self):
+        return self.count
+    def insert(self,item):
+        new_node = Node(item)
+        new_node.next = self.head
+        self.head = new_node
+        self.count +=1
+    def display(self):
+        all_ele = self.head
+        while all_ele.getNext() != None:
+            print(all_ele.getItem() , all_ele.getNext())
+            all_ele = all_ele.getNext()
+            
+    def is_empty(self):
+        return self.head == None
+        
+    def del_all(self):
+        self.head = None
+        return "flushed"
+    
+    def find(self ,item):
+        all_ele = self.head
+        while all_ele.getNext() != None:
+            if all_ele.getItem() == item:
+                return "item present "
+            
+            all_ele = all_ele.next
+        return "item not present"
+
+x = LinkedList()
+for i in range(40):
+    x.insert(i)
+
+x.display()
